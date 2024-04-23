2ed377a4118966848c7fee3f16eb655c159a5cb7
Tawhid Sajib <tauhidulsajib34@gmail.com>
2024-04-23 17:56:49 +0600

    mobile responsive added

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,14 +20,3 @@ faqItems.forEach(item => {
         }
     });
 });
-
-
-new WOW().init();
-
-
-  	/* AUTHOR LINK */
-     $('.about-me-img').hover(function(){
-            $('.authorWindowWrapper').stop().fadeIn('fast').find('p').addClass('trans');
-        }, function(){
-            $('.authorWindowWrapper').stop().fadeOut('fast').find('p').removeClass('trans');
-        });
